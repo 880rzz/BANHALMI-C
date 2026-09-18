8d47f762d0e52ecad4f3320531993b7103819efb
V679643 <107166660+880rzz@users.noreply.github.com>
2026-09-18 22:29:50 +0200

    Make lazy mega-menu core handoff deterministic

--- assets/js/mega-menu.js
+++ assets/js/mega-menu.js
@@ -6,5 +6,12 @@
   core.src='/assets/js/mega-menu-v65-base.js?v=20260917-pricing-service-v32';
   core.defer=true;
   core.setAttribute('data-banhalmi-mega-menu-core','');
+  core.onload=function(){
+    var button=document.querySelector('.menu-btn');
+    var menu=document.getElementById('bn-mega-menu');
+    if(button && document.activeElement===button && menu && menu.hidden){
+      setTimeout(function(){ button.click(); },0);
+    }
+  };
   document.head.appendChild(core);
 })();
